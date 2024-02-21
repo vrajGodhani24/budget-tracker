82a36333f2843f7e2db429ebfefe4ad31fd58f4a
AjayToliya <toliyaajay2212@gmail.com>
2024-02-21 19:26:30 +0530

    21/02/24 First Upoad

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:expence_tracker/module/views/homepage/homepage.dart';
+import 'package:expence_tracker/module/views/splash/screens/splashscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'splash',
+      getPages: [
+        GetPage(name: '/', page: () => const HomePage()),
+        GetPage(name: '/splash', page: () => const SplashScreen()),
+      ],
+    ),
+  );
+}
---
+++ lib/module/views/homepage/homepage.dart
@@ -0,0 +1,120 @@
+import 'package:expence_tracker/module/utils/category.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.white,
+              Colors.lightGreen.shade300,
+            ],
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CustomScrollView(
+                  slivers: [
+                    const SliverAppBar(
+                      title: Text(
+                        "Categories",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      backgroundColor: Colors.transparent,
+                    ),
+                    SliverGrid(
+                      delegate: SliverChildBuilderDelegate(
+                        childCount: allCategory.length,
+                        (context, index) => Card(
+                          color: Colors.lightGreen.withOpacity(0.2),
+                          child: Container(
+                            padding: const EdgeInsets.all(6),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(18),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Image.asset(
+                                  allCategory[index]['categoryImage'],
+                                  height: 80,
+                                ),
+                                Text(
+                                  "${allCategory[index]['categoryName']}",
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 4,
+                        mainAxisSpacing: 4,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  color: Colors.black,
+                ),
+                child: BottomNavigationBar(
+                  elevation: 0,
+                  type: BottomNavigationBarType.shifting,
+                  selectedItemColor: Colors.white,
+                  unselectedItemColor: Colors.lightGreen,
+                  items: const [
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.category),
+                      label: 'Add Category',
+                      backgroundColor: Colors.transparent,
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.monetization_on),
+                      label: 'Add Budget',
+                      backgroundColor: Colors.transparent,
+                    ),
+                    BottomNavigationBarItem(
+                      icon: Icon(Icons.history),
+                      label: 'Budget History',
+                      backgroundColor: Colors.transparent,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
